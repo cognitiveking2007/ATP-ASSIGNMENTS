c2cb8a27990291177b6e5e5d29759fb599244c2d
cognitiveking2007 <ratakondalalith@gmail.com>
2026-02-28 22:05:36 +0530

    Add files via upload

---
+++ array-iteration.js
@@ -0,0 +1,12 @@
+
+let emps=[
+    {eno:1,name:"bhanu"},
+    {eno:2,name:"vikas"},
+    {eno:3,name:"varun"}
+];
+//iterate the emps array andd print eno&ename of each emp
+//eno is 1 and name is
+for(v of emps){
+    console.log(`eno is ${v.eno} and ename is ${v.name}`)
+    //console.log 
+}
